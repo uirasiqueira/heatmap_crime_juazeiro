413f769840898b887d1fbccbb5abd22b7681f494
Uirá Siqueira <54291159+uirasiqueira@users.noreply.github.com>
2025-11-20 17:57:37 -0300

    Update heatmap_criminalidade.py

--- heatmap_criminalidade.py
+++ heatmap_criminalidade.py
@@ -123,7 +123,7 @@ for idx, row in df_juazeiro.iterrows():
         <b>Data:</b> {row['DATA_FATO']}<br>
         <b>Hora:</b> {row['HORA_FATO']}<br>
         <b>Idade:</b> {row['IDADE']}<br>
-        <b>Agente (iniciais):</b> {row['INICIAIS']}<br>
+        <b>Vítima (iniciais):</b> {row['INICIAIS']}<br>
         <b>Ocupação:</b> {row['OCUPACAO']}
         """
 
