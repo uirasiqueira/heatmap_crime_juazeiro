5699268adc75eaf0abd1556c9dd3323efb03772d
Uirá Siqueira <54291159+uirasiqueira@users.noreply.github.com>
2025-11-23 16:33:03 -0300

    Update heatmap_criminalidade.py

--- heatmap_criminalidade.py
+++ heatmap_criminalidade.py
@@ -8,6 +8,7 @@ from streamlit.components.v1 import html
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 import uuid
+import json
 
 
 # ======================================================
